7f81a64b2d623c3b0511b14224a45e481ea05e19
kgarcia7 <kgarcia7814@gmail.com>
2024-02-01 17:30:53 -0500

    Setting up work section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import KGimage from '../assets/KGimage.png'
+import KGimage from "../assets/KGimage.png";
 
 export const About = () => {
   return (
@@ -7,22 +7,33 @@ export const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-white">About</p>
+            <p className="text-4xl font-bold inline border-b-4 border-white">
+              About
+            </p>
           </div>
           <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi. I'm Karina, nice to meet you. Please take a look around. </p>
+            <img className="max-h-[600px] w-full" src={KGimage} />
           </div>
-          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-            <div className="sm:text-right text-4xl font-bold">
-              <p>Hi. I'm Karina, nice to meet you. Please take a look around. </p>
-              <img className='max-h-[500px] w-full h-screen' src={KGimage} />
-            </div>
-            <div className="text-black">
-              <p >As a career changer transitioning into the dynamic realm of full stack web development, I bring a diverse background in health administration, with a particular focus on revenue cycle management analysis. With invaluable experience gained at Flatiron Health, a leading software development company, I've honed my analytical skills and cultivated a keen eye for optimizing processes and enhancing operational efficiency. Motivated by a passion for innovation and technology, I'm eager to leverage my expertise in a new capacity, merging my healthcare insights with the exciting challenges of web development. With a strong foundation in problem-solving and a relentless drive for excellence, I am poised to embark on this transformative journey, committed to crafting seamless digital solutions that positively impact lives.
-              </p>
-            </div>
+          <div className="text-[#5C8D89]">
+            <p>
+              As I transition into the dynamic realm of full stack web development, I bring with me a diverse background in health administration, where my focus centered on Revenue Cycle Management analysis.
+            </p>
+
+            <p className="mt-4">
+              During my tenure at Flatiron Health, a prominent software development company, I sharpened my analytical skills and refined my ability to optimize processes, fostering operational efficiency. Fueled by a deep-seated passion for innovation and technology, I'm excited to channel my expertise into web development, seamlessly merging my healthcare insights with the exhilarating challenges of this field.
+            </p>
+
+            <p className="mt-4">
+              With a solid foundation in problem-solving and an unwavering commitment to excellence, I am poised to embark on this transformative journey. My goal is to build seamless digital solutions that positively impact lives, driven by my passion for coding and my relentless pursuit of growth.
+            </p>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,172 @@
-import React from 'react'
+import React from "react";
+import WorkImg from "../assets/workimg.png";
 
 const Work = () => {
   return (
-    <div>
+    <div name="work" className="w-full md:h-screen text-black bg-[#A7D7C5]">
+      <div className="max-w-[-1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-[#5C8D89] border-white">
+            Work
+          </p>
+          <p className="py-6">Check out some of my recent work</p>
+        </div>
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Front-End Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://codyccl.github.io/WellmatesProject-1/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/CodyCCL/WellmatesProject-1.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
 
+        {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Front-End Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://codyccl.github.io/WellmatesProject-1/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/CodyCCL/WellmatesProject-1.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        {/* Grid Item */}
+            <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Front-End Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://codyccl.github.io/WellmatesProject-1/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/CodyCCL/WellmatesProject-1.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Front-End Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://codyccl.github.io/WellmatesProject-1/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/CodyCCL/WellmatesProject-1.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Front-End Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://codyccl.github.io/WellmatesProject-1/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/CodyCCL/WellmatesProject-1.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Front-End Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://codyccl.github.io/WellmatesProject-1/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/CodyCCL/WellmatesProject-1.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-black">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
